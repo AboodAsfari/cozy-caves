b6d97e2f31c807e10f9573098fd2323926087d83
Gavin Lim <gavinlim04@gmail.com>
2023-08-09 19:42:13 +1200

    Fixed Bug with BSP algorithm

--- dungeon-generation/DungeonGen.js
+++ dungeon-generation/DungeonGen.js
@@ -1,9 +1,10 @@
-let height = 50;
-let width = 50;
-const mapGrid = Array.from({length: height}, () => Array.from({length: width}, () => 0));
+let height = 100;
+let width = 100;
+const mapGrid = Array.from({length: height}, () => Array.from({length: width}, () => '_'));
 
 function bsp(mapGrid, x, y, w, h, recursions) {
-    if (recursions <= 0) {
+    console.log(recursions + ":" + x + "," + y + "," + w + "," + h);
+    if (recursions <= 0 || w < 4 || h < 4) {
         return;
     }
 
@@ -11,16 +12,17 @@ function bsp(mapGrid, x, y, w, h, recursions) {
 
     if (splitHorizontal) {
         const splitPosition = Math.floor(Math.random() * (h - 2)) + 1 + y;
+        console.log(splitPosition);
         for(let i = x; i < x + w; i++) {
-            mapGrid[splitPosition][i] = 1;
+            mapGrid[splitPosition][i] = '#';
         }
         bsp(mapGrid, x, y, w, splitPosition - y, recursions - 1);
         bsp(mapGrid, x, splitPosition + 1, w, h - (splitPosition - y) - 1, recursions - 1);
     } else {
         const splitPosition = Math.floor(Math.random() * (w - 2)) + 1 + x;
-        for(let i = x; i < x + w; i++) {
-            mapGrid[i][splitPosition] = 1;
-            
+        for(let i = y; i < y + h; i++) {
+            mapGrid[i][splitPosition] = '#';
+
         }
         bsp(mapGrid, x, y, splitPosition - x, h, recursions - 1);
         bsp(mapGrid, splitPosition + 1, y, w - (splitPosition - x) - 1, h, recursions - 1);
@@ -32,8 +34,8 @@ bsp(mapGrid, 0, 0, width, height, 4);
 
 for (let i = 0; i < height; i++) {
     // Join the elements in the row to form a string representation
-    const rowString = mapGrid[i].join(', ');
-
+    const rowString = mapGrid[i].join(' ');
+    
     // Print the row
     console.log(`${rowString}`);
   }
