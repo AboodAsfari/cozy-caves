0090068a3928615d3fc75d5b75c0ce91a8889876
Carlo Cigaral <carlo.cigaral@gmail.com>
2023-08-10 19:51:52 +1200

    Modified random split position generation and added comments throughout

--- dungeon-generation/DungeonGen.js
+++ dungeon-generation/DungeonGen.js
@@ -1,25 +1,37 @@
-let height = 100;
-let width = 100;
+let height = 175; //Will need tuning to find "preset" values
+let width = 175; //Will need tuning to find "preset" values
 const mapGrid = Array.from({length: height}, () => Array.from({length: width}, () => '_'));
+let minGap = 5; //Will need tuning to find "preset" values
+let maxDepth = 10; //Will need tuning to find "preset" values
+let splitHorizontal = Math.random() > 0.5;
+
+//Keeping range for random partition placement more consistent for better spacing
+function generateSplitPosition(min, max, minDistance) {
+    const range = max - min - 2 * minDistance + 1;
+    const startPosition = min + minDistance;
+    return startPosition + Math.floor(Math.random() * range);
+}
 
 function bsp(mapGrid, x, y, w, h, recursions) {
-    console.log(recursions + ":" + x + "," + y + "," + w + "," + h);
-    if (recursions <= 0 || w < 4 || h < 4) {
+    // console.log(recursions + ":" + x + "," + y + "," + w + "," + h); // TESTING PRINT STATEMENT
+    //Makes algorithm more likely to alternate between vertical and horizontal partition - More consistent even spread of open spaces
+    splitHorizontal = splitHorizontal ? splitHorizontal = Math.random() > 0.8 : splitHorizontal = Math.random() > 0.2;
+    
+    //Exits if max depth reached OR if width/height is less than 2x+1 to ensure min room size is not too small
+    if (recursions <= 0 || (w < (minGap*2 + 1) && !splitHorizontal) || (h < (minGap*2 +1) && splitHorizontal)) {
         return;
     }
-
-    const splitHorizontal = Math.random() > 0.5;
-
+    
     if (splitHorizontal) {
-        const splitPosition = Math.floor(Math.random() * (h - 2)) + 1 + y;
-        console.log(splitPosition);
+        const splitPosition = generateSplitPosition(y, y + h, minGap);
+        // console.log(splitPosition); // TESTING PRINT STATEMENT
         for(let i = x; i < x + w; i++) {
             mapGrid[splitPosition][i] = '#';
         }
         bsp(mapGrid, x, y, w, splitPosition - y, recursions - 1);
         bsp(mapGrid, x, splitPosition + 1, w, h - (splitPosition - y) - 1, recursions - 1);
     } else {
-        const splitPosition = Math.floor(Math.random() * (w - 2)) + 1 + x;
+        const splitPosition = generateSplitPosition(x, x + w, minGap);
         for(let i = y; i < y + h; i++) {
             mapGrid[i][splitPosition] = '#';
 
@@ -30,12 +42,19 @@ function bsp(mapGrid, x, y, w, h, recursions) {
     }
 }
 
-bsp(mapGrid, 0, 0, width, height, 4);
+//TESTING SETUP - Generates 50 random BSP maps
+for(let j = 0; j < 50; j++){
+    const mapGrid = Array.from({length: height}, () => Array.from({length: width}, () => '_'));
 
-for (let i = 0; i < height; i++) {
-    // Join the elements in the row to form a string representation
-    const rowString = mapGrid[i].join(' ');
-    
-    // Print the row
-    console.log(`${rowString}`);
-  }
+    bsp(mapGrid, 0, 0, width, height, maxDepth);
+
+    for (let i = 0; i < height; i++) {
+        // Join the elements in the row to form a string representation
+        const rowString = mapGrid[i].join(' ');
+        
+        // Print the row
+        console.log(`${rowString}`);
+    }
+    console.log("");
+    console.log("");
+}
