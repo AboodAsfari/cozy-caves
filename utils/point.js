8bb8358dec92221bd81a8bddb0a541e1852a1d74
AboodAsfari <abdz2004@gmail.com>
2023-08-13 13:59:39 +1200

    Added method to check if point is valid

--- utils/point.js
+++ utils/point.js
@@ -7,6 +7,8 @@ class Point {
         this.#y = y;
     }
 
+    static isNonZeroPoint(point) { return point instanceof Point && point.getX() > 0 && point.getY() > 0; }
+
     getX() { return this.#x; }
     getY() { return this.#y; }
 
