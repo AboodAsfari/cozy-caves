9c20a434398c36df1f9df907b10b910cd2f2710f
AboodAsfari <abdz2004@gmail.com>
2023-08-13 14:21:40 +1200

    Added point setters and clone method

--- utils/point.js
+++ utils/point.js
@@ -10,10 +10,21 @@ class Point {
 
     static isPositivePoint(point) { return point instanceof Point && point.getX() > 0 && point.getY() > 0; }
 
+    setX(x) { 
+        if (!Number.isInteger(x)) throw new Error('Invalid X coordinate provided.');
+        this.#x = x; 
+    }
+    setY(y) {
+        if (!Number.isInteger(y)) throw new Error('Invalid Y coordinate provided.');
+        this.#y = y;
+    }
+
     getX() { return this.#x; }
     getY() { return this.#y; }
 
     toString() { return this.x + "," + this.y; }
+
+    clone() { return new Point(this.#x, this.#y); }
 }
 
 module.exports = Point;
