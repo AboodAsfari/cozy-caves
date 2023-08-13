4048cf28eba473424c03e49be1c3faaeef411740
AboodAsfari <abdz2004@gmail.com>
2023-08-13 23:45:41 +1200

    Added tile comments

--- room-generation/src/tile/tile.js
+++ room-generation/src/tile/tile.js
@@ -1,18 +1,31 @@
 const Point = require("../../../utils/point");
 
+/**
+ * Represents a tile in the room.
+ * 
+ * @author Abdulrahman Asfari
+ */
 class Tile {
-    #tileType;
-    #position;
-    #offset = new Point(0, 0);
-    #rotation = 0;
-    #depth = 0;
+    #tileType; // Type of tile to display.
+    #position; // Position of the tile in the room.
+    #offset = new Point(0, 0); // Rendering offset.
+    #rotation = 0; // Rendering rotation.
+    #depth = 0; // Rendering depth.
 
+    /**
+     * Creates an instance of Tile.
+     *
+     * @constructor
+     * @param tileType Type of tile, e.g. "floor".
+     * @param position Position of the tile in the room.
+     */
     constructor(tileType, position) {
         if (!tileType || !(position instanceof Point)) throw new Error('Invalid tile provided.');
         this.#tileType = tileType.toString();
         this.#position = position;
     }
 
+    // Getters.
     getTileType() { return this.#tileType; }
     getPosition() { return this.#position; }
     getOffset() { return this.#offset; }
--- room-generation/src/tile/tileSource.js
+++ room-generation/src/tile/tileSource.js
@@ -1,15 +1,28 @@
 const Point = require("../../../utils/point");
 
+/**
+ * Represents an image source / image dimensions pair.
+ * 
+ * @author Abdulrahman Asfari
+ */
 class TileSource {
-    #imgSource;
+    #imgSource; // String
     #dimensions;
 
+    /**
+     * Creates an instance of TileSource.
+     * 
+     * @constructor
+     * @param imgSource String containing an image source.
+     * @param dimensions Dimensions of the image.
+     */
     constructor(imgSource, dimensions) {
         if (!imgSource || Point.isPositivePoint(dimensions)) throw new Error('Invalid source provided.');
         this.#imgSource = imgSource.toString();
         this.#dimensions = dimensions;
     }
 
+    // Getters.
     getDimensions() { return this.#dimensions; }
     getImage() { return this.#imgSource; }
 }
