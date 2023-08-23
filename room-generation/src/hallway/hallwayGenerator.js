373cf80929c27ef5fb02fab430cc444051d248d5
Gavin Lim <limgavi@ecs.vuw.ac.nz>
2023-08-23 18:41:03 +1200

    Added some file structure for hallway generation

--- room-generation/src/hallway/hallwayGenerator.js
+++ room-generation/src/hallway/hallwayGenerator.js
@@ -1,1 +1,15 @@
-import Delaunator from 'delaunator';
+const Delanuator = require('delaunator');
+
+function getPointsFromRooms() {
+    //gets centre points from room
+}
+
+function generateHallways(rooms) {
+    for (const room in rooms) {
+        log(room.toString());
+    }
+
+}
+
+module.exports = generateHallways;
+
--- room-generation/tests/hallway.test.js
+++ room-generation/tests/hallway.test.js
@@ -1,6 +1,30 @@
 const log = require("console").log;
+const Point = require("@cozy-caves/utils").Point;
+const RoomBuilder = require("../src/room/roomBuilder");
+const generateHallways = require("../src/hallway/hallwayGenerator");
 
-test('Example hallway', () => {
-    log("GOOD");
-    expect(true).toBe(true);
+let rooms;
+
+test("hallway generation", () => {
+    test('Rect room generation', () => {
+        let testInputs = [
+            { size: new Point(5, 5), leniency: new Point(0, 0) },
+            { size: new Point(7, 7), leniency: new Point(0, 0) },
+            { size: new Point(6, 6), leniency: new Point(0, 0) },
+            { size: new Point(8, 8), leniency: new Point(0, 0) },
+            { size: new Point(8, 8), leniency: new Point(0, 0) },
+            { size: new Point(6, 6), leniency: new Point(0, 0) },
+            { size: new Point(5, 5), leniency: new Point(0, 0) },
+        ];
+    
+        log("----------------------------------------------------------------");
+        for (const testInput of testInputs) {
+            log("Generating room with expected size: " + testInput.size.toString() + ", and leniency: " + testInput.leniency.toString());
+    
+            let room = new RoomBuilder().setSize(testInput.size).setLeniency(testInput.leniency).build();
+            rooms.push(room);
+            
+        }
+        generateHallways(rooms);
+    });
 });
