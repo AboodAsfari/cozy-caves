3625144ddc84333e89bfafb8fc87a8cf255470e2
Gideon Wilkins <wilkingide@ecs.vuw.ac.nz>
2023-08-23 19:52:50 +1200

    Implemented basic zoom and drag functionality

--- webapp/src/renderer/RendererCanvas.jsx
+++ webapp/src/renderer/RendererCanvas.jsx
@@ -1,13 +1,84 @@
-import { React, useState } from 'react';
+import  React from 'react';
 import { Point } from "@cozy-caves/utils";
-import { Stage, Sprite } from '@pixi/react';
+import { Stage, Sprite, PixiComponent, useApp } from '@pixi/react';
 import { BaseTexture, SCALE_MODES } from 'pixi.js';
+import { EventSystem } from "@pixi/events"; 
+import { RoomBuilder } from "@cozy-caves/room-generation";
 
-const RoomBuilder = require("@cozy-caves/room-generation");
+const { useState, useEffect, useRef, forwardRef } = React;
+const Viewport = require('pixi-viewport');
 
 const RendererCanvas = (props) => {
+
   BaseTexture.defaultOptions.scaleMode = SCALE_MODES.NEAREST
-  
+
+  const stageOptions = {
+    antialias: true,
+    autoDensity: true,
+    backgroundColor: '0xefefef',
+  };
+
+  const useResize = () => {
+    const [size, setSize] = useState([window.innerWidth, window.innerHeight]);
+    
+    useEffect(() => {
+      const onResize = () => {
+        requestAnimationFrame(() => {
+          setSize([window.innerWidth, window.innerHeight])        
+        })
+      };
+      
+      window.addEventListener('resize', onResize);
+      
+      return () => {
+        window.removeEventListener('resize', onResize);
+      }
+    }, []);
+    
+    return size;
+  };
+
+  // create and instantiate the viewport component
+  // we share the ticker and interaction from app
+  const PixiViewportComponent = PixiComponent("Viewport", { 
+    create(props) {
+      const { app, ...viewportProps } = props;
+
+      const viewport = new Viewport.Viewport({
+        ticker: props.app.ticker,
+        events: app.renderer.events,
+        interaction: props.app.renderer.plugins.interaction,
+        ...viewportProps
+      });
+
+      // activate plugins
+      (props.plugins || []).forEach((plugin) => {
+        viewport[plugin]();
+      });
+
+      return viewport;
+    },
+    applyProps(viewport, _oldProps, _newProps) {
+      const { plugins: oldPlugins, children: oldChildren, ...oldProps } = _oldProps;
+      const { plugins: newPlugins, children: newChildren, ...newProps } = _newProps;
+
+      Object.keys(newProps).forEach((p) => {
+        if (oldProps[p] !== newProps[p]) {
+          viewport[p] = newProps[p];
+        }
+      });
+    },
+    didMount() {
+      console.log("viewport mounted");
+    }
+  });
+
+  // create a component that can be consumed
+  // that automatically pass down the app
+  const PixiViewport = forwardRef((props, ref) => (
+    <PixiViewportComponent ref={ref} app={useApp()} {...props} />
+  ));
+
   const dimensions = new Point(10, 10);
 
   const [ room, setRoom ] = useState(new RoomBuilder().setSize(dimensions).setLeniency(new Point(0, 0)).build());
@@ -22,11 +93,28 @@ const RendererCanvas = (props) => {
     return <Sprite image={"resources/"+img+".jpg"} scale={{x:scaleX, y:scaleY}} x={((tile.getPosition().getX() * size)*scaleX) + offsetX} y={(tile.getPosition().getY() * size)*scaleY + offsetY} />
   }
 
+  // get the actual viewport instance
+  const viewportRef = useRef();
+
+  // get the current window size
+  const [width, height] = useResize();
+
   return (
-      <Stage width={window.innerWidth} height={window.innerHeight} options={{backgroundColor:'#433a3b'}} >
+    <>
+      <Stage width={width} height={height} options={stageOptions}>
+        <PixiViewport
+          ref={viewportRef}
+          plugins={["drag", "pinch", "wheel", "decelerate"]}
+          screenWidth={width}
+          screenHeight={height}
+          worldWidth={width * 4}
+          worldHeight={height * 4}
+        >
         { room.getTiles().map((tile) => drawTile(tile)) }
+        </PixiViewport>
       </Stage>
-    );
+    </>
+  );
 };
 
 export default RendererCanvas;
