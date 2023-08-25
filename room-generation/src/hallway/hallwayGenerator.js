b234fa8f7858444c24b7dbc2667779030710fc39
Gavin Lim <limgavi@ecs.vuw.ac.nz>
2023-08-25 16:55:15 +1200

    Correctly calculates Triangulation.

--- room-generation/src/hallway/hallwayGenerator.js
+++ room-generation/src/hallway/hallwayGenerator.js
@@ -1,14 +1,41 @@
-const Delanuator = require('delaunator');
+const Delaunator = require('delaunator');
+const Room = require('../room/room');
+const Point = require("@cozy-caves/utils").Point;
 
-function getPointsFromRooms() {
-    //gets centre points from room
+function getmidPoint(room) {
+    let point = room.getPosition();
+    let x = point.getX();
+    let y = point.getY();
+    let dims = room.getDimensions();
+    let w = dims.getX();
+    let h = dims.getY();
+    return new Point(x + Math.round((w/2)), y + Math.round(h/2));
 }
 
 function generateHallways(rooms) {
-    for (const room in rooms) {
-        log(room.toString());
+    const midpoints = [];
+    for (let key in rooms) {
+        if(rooms[key] instanceof Room) {
+            point = getmidPoint(rooms[key]);
+            midpoints.push(point.getX());
+            midpoints.push(point.getY());
+        }
     }
+    console.log(midpoints);
+    const delaunay = new Delaunator(midpoints);
+    let triangles = delaunay.triangles;
+    console.log(triangles);
 
+    let coordinates = [];
+    for (let i = 0; i < triangles.length; i += 3) {
+        let points = [];
+        coordinates.push([
+            points[triangles[i]],
+            points[triangles[i + 1]],
+            points[triangles[i + 2]]
+        ]);
+    }
+    console.log(coordinates);
 }
 
 module.exports = generateHallways;
