4e53bcdd8b7e47c1b94cf622b97fd9bdb3eb9784
Gavin Lim <limgavi@ecs.vuw.ac.nz>
2023-08-25 15:22:46 +1200

    Updated tests to be correct

--- room-generation/tests/hallway.test.js
+++ room-generation/tests/hallway.test.js
@@ -3,28 +3,28 @@ const Point = require("@cozy-caves/utils").Point;
 const RoomBuilder = require("../src/room/roomBuilder");
 const generateHallways = require("../src/hallway/hallwayGenerator");
 
-let rooms;
+let rooms = [];
 
 test("hallway generation", () => {
-    test('Rect room generation', () => {
-        let testInputs = [
-            { size: new Point(5, 5), leniency: new Point(0, 0) },
-            { size: new Point(7, 7), leniency: new Point(0, 0) },
-            { size: new Point(6, 6), leniency: new Point(0, 0) },
-            { size: new Point(8, 8), leniency: new Point(0, 0) },
-            { size: new Point(8, 8), leniency: new Point(0, 0) },
-            { size: new Point(6, 6), leniency: new Point(0, 0) },
-            { size: new Point(5, 5), leniency: new Point(0, 0) },
-        ];
-    
-        log("----------------------------------------------------------------");
-        for (const testInput of testInputs) {
-            log("Generating room with expected size: " + testInput.size.toString() + ", and leniency: " + testInput.leniency.toString());
-    
-            let room = new RoomBuilder().setSize(testInput.size).setLeniency(testInput.leniency).build();
-            rooms.push(room);
-            
-        }
-        generateHallways(rooms);
-    });
+    let testInputs = [
+        { size: new Point(5, 5), leniency: new Point(0, 0) },
+        { size: new Point(7, 7), leniency: new Point(0, 0) },
+        { size: new Point(6, 6), leniency: new Point(0, 0) },
+        { size: new Point(8, 8), leniency: new Point(0, 0) },
+        { size: new Point(8, 8), leniency: new Point(0, 0) },
+        { size: new Point(6, 6), leniency: new Point(0, 0) },
+        { size: new Point(5, 5), leniency: new Point(0, 0) },
+    ];
+
+    log("----------------------------------------------------------------");
+    for (const testInput of testInputs) {
+        log("Generating room with expected size: " + testInput.size.toString() + ", and leniency: " + testInput.leniency.toString());
+
+        let room = new RoomBuilder().setSize(testInput.size).setLeniency(testInput.leniency).build();
+        room.setPosition(new Point(0,0));
+        rooms.push(room);
+        
+    }
+    console.log(rooms[0].toString());
+    generateHallways(rooms);
 });
