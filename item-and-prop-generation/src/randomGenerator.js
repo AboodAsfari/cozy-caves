7fc2e2e5d7e9f3b4d8d0e2666054d8161982f419
Naomi-Parte <naomiparte@gmail.com>
2023-08-29 22:14:18 +1200

    returning a randomGenerator class from a function

--- item-and-prop-generation/src/randomGenerator.js
+++ item-and-prop-generation/src/randomGenerator.js
@@ -1,43 +1,77 @@
 const PropGenerator = require('./propGenerator.js');
 const Room = require('../../room-generation/src/room');
 
+class RandomGenerator {
+    #populatedRoom = new Map();
+    #room;
 
-function getRandomRarity() {
-    const rand = Math.random() * 100 + 1;
-
-    if (rand <= 60) { // common
-        return PropGenerator.rarityList[0];
-    } else if (rand <= 90) { // uncommon
-        return PropGenerator.rarityList[1];
-    } else if (rand <= 100) { // rare
-        return PropGenerator.rarityList[2];
-    } else { // just in case something goes wrong, go with common
-        return PropGenerator.rarityList[0];
+    constructor (room) {
+        if (!(room instanceof Room)) throw new Error("Invalid type. Expecting a room object.");
+        this.#room = room;
     }
-}
 
-
-function populateRoom(room, maxProp) {
-    const populatedRoom = new Map();
-    // simplest logic for now
-    if (!(room instanceof Room)) throw new Error("Invalid type. Expecting a room object.");
-
-    const tiles = room.getTiles();
+    getProp(pos) {
+        return this.#populatedRoom.get(pos.toString());
+    }
     
-    for (let i=0; i<room.getDimensions().getX(); i++) {
-        for (let j=0; j<room.getDimensions().getY(); j++) {
-            let pos = new Point(i, j).toString();
-            let tile = room.getTile(pos);
-            if (tile.getTileType() === "floor") {
-                let rand = Math.random();
+    getRandomRarity() {
+        const rand = Math.random() * 100 + 1;
+    
+        if (rand <= 60) { // common
+            return PropGenerator.rarityList[0];
+        } else if (rand <= 90) { // uncommon
+            return PropGenerator.rarityList[1];
+        } else if (rand <= 100) { // rare
+            return PropGenerator.rarityList[2];
+        } else { // just in case something goes wrong, go with common
+            return PropGenerator.rarityList[0];
+        }
+    }
+    
+    getPopulatedRoom(maxProp) {
+        for (let i=0; i<this.#room.getDimensions().getX(); i++) {
+            for (let j=0; j<this.#room.getDimensions().getY(); j++) {
+                let pos = new Point(i, j).toString();
+                let tile = this.#room.getTile(pos);
 
-                // will put props 40% of the time in the room
-                if (rand < 0.4 && populatedRoom.size() <= maxProp) { 
-                    populatedRoom.set(pos, PropGenerator.getPropByRarity(getRandomRarity()));
+                // makes sure to generate props only on the floor not wall
+                if (tile.getTileType() === "floor") {
+                    let rand = Math.random();
+
+                    // will put props 40% of the time in the room
+                    if (rand < 0.4 && this.#populatedRoom.size() <= maxProp) { 
+                        this.#populatedRoom.set(pos, PropGenerator.getPropByRarity(getRandomRarity()));
+                    }
                 }
             }
         }
+        return this.#room;
     }
-    return populatedRoom;
+
+    toString() {
+        let roomArray = [];
+        let dimensions = this.#room.getDimensions();
+
+        for (let i = 0; i < dimensions.getY(); i++) {
+            roomArray.push("");
+            for (let j = 0; j < dimensions.getX(); j++) {
+                let tile = this.#room.getTile(new Point(j, i).toString());
+                let prop = this.getProp(new Point(j, i).toString());
+                if (prop !== null) roomArray[i] += "P";
+                else if (!tile) roomArray[i] += "X";
+                else if (tile.getTileType() === "floor") roomArray[i] += "O";
+                else roomArray[i] += "I";
+                roomArray[i] += "  ";
+            }
+        }
+        let finalString = roomArray.join("\n");
+        return finalString.substring(0, finalString.length - 1);
+    }
+
 }
+
+function populateRoom(room) {
+    return new RandomGenerator(room);
+}
+
 module.exports = populateRoom;
