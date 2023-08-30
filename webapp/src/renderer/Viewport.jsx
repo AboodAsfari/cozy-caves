55bad73a56490da2e5c33caf2bedd121e396eb64
Gideon Wilkins <wilkingide@ecs.vuw.ac.nz>
2023-08-30 18:52:47 +1200

    Initial implementation, very bad, redo completely

--- webapp/src/renderer/Viewport.jsx
+++ webapp/src/renderer/Viewport.jsx
@@ -7,16 +7,16 @@ import { Viewport as PixiViewport } from "pixi-viewport";
 // we share the ticker and interaction from app
 const PixiViewportComponent = PixiComponent("Viewport", { 
     create(props) {
-        const { app, ...viewportProps } = props;
+        const { app, maxX, maxY,...viewportProps } = props;
         
         // comes from github issue: https://github.com/davidfig/pixi-viewport/issues/438
         // Install EventSystem, if not already
         // (PixiJS 6 doesn't add it by default)
         const events = new EventSystem(props.app.renderer);
-        events.domElement = props.app.renderer.view;
+        events.domElement = app.renderer.view;
 
         const viewport = new PixiViewport({
-        ticker: props.app.ticker,
+        ticker: app.ticker,
         events: events,
         disableOnContextMenu: true,
         sortableChildren: true,
@@ -27,15 +27,21 @@ const PixiViewportComponent = PixiComponent("Viewport", {
             friction: 0.90,
         })
         .clampZoom({
-            minScale: 0.1,
+            minScale: (maxX > maxY ? viewportProps.screenWidth/maxX : viewportProps.screenHeight/maxY) / 2,
             maxScale: 8,
         });
-
+        viewport.moveCenter(maxX/2, maxY/2);
+        if(maxX >= maxY) {
+            viewport.fitWidth(maxX*2, true, true, true);
+        }
+        if(maxY > maxX) {
+            viewport.fitHeight(maxY*2, true, true, true);
+        }
         return viewport;
     },
 
     // comes from github issue: https://github.com/davidfig/pixi-viewport/issues/438
-    willUnmount(instance, parent) {
+    willUnmount(instance) {
         // workaround because the ticker is already destroyed by this point by the stage
         instance.options.noTicker = true;
         instance.destroy({children: true, texture: true, baseTexture: true})
