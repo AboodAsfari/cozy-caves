4390fb5989cdb768033ba79b7453261cb19ed1e3
AboodAsfari <abdz2004@gmail.com>
2023-08-30 23:08:00 +1200

    Added color picker for partitions

--- layout-editor/src/PartitionPanel/PartitionIconPopover.jsx
+++ layout-editor/src/PartitionPanel/PartitionIconPopover.jsx
@@ -14,6 +14,8 @@ import "../styles/PartitionPanel.css";
 
 import iconMap from "../PartitionIcons";
 
+import { HexColorPicker, HexColorInput } from "react-colorful";
+
 const PartitionIconPopover = (props) => {
   const {
     partition,
@@ -22,26 +24,47 @@ const PartitionIconPopover = (props) => {
     update
   } = props;
 
+  const [colorMenu, setColorMenu] = React.useState(false);
+  const counter = React.useRef(0);
+
+    React.useEffect(() => {
+        if (!!iconAnchor) setColorMenu(false);
+    }, [iconAnchor]);
+
   return (
-    <Popover className="PartitionIconPanel" open={!!iconAnchor} anchorEl={iconAnchor} onClose={() => setIconAnchor(null)} sx={{ mt: 1 }}
+    <Popover className="PartitionIconPanel" open={!!iconAnchor} anchorEl={iconAnchor} onClose={() => { setIconAnchor(null); }} sx={{ mt: 1 }}
         anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }} transformOrigin={{ vertical: 'top', horizontal: 'center' }}>
-        <Stack sx={{ alignItems: "center", justifyContent: "space-evenly", width: "300px" }}>
+        <Stack sx={{ alignItems: "center", justifyContent: "space-evenly", width: "320px" }}>
             <Stack direction="row" spacing={3}>
-                <Button disableRipple sx={{ textTransform: "none", color: "white", fontSize: 20, "&:hover": { backgroundColor: "transparent" } }}> Icon </Button>
-                <Button disableRipple sx={{ textTransform: "none", color: "white", fontSize: 20, "&:hover": { backgroundColor: "transparent" } }}> Color </Button>
+                <Button disableRipple onClick={() => setColorMenu(false)} sx={{ textTransform: "none", color: "white", fontSize: 20, "&:hover": { color: "#7da36d", backgroundColor: "transparent" } }}> Icon </Button>
+                <Button disableRipple onClick={() => setColorMenu(true)} sx={{ textTransform: "none", color: "white", fontSize: 20, "&:hover": { color: "#7da36d", backgroundColor: "transparent" } }}> Color </Button>
             </Stack>
-            <Divider flexItem sx={{ mb: 2 }} />
-            <Grid container sx={{ alignItems: "center", justifyContent: "space-evenly" }}>
-                {Object.keys(iconMap).map((key) => (
-                    <Grid item key={key} xs={4} sx={{ display: "flex", alignItems: "center", justifyContent: "center", mb: 2 }} 
-                        onClick={() => { partition.setPartitionIcon(key); update(); setIconAnchor(null); }}>
-                        <Box className="PartitionIcon">
-                            {iconMap[key]}
-                        </Box>
-                    </Grid>
-                ))}
-                
-            </Grid>
+            <Divider flexItem sx={{ mb: 0 }} />
+            <Box sx={{ height: "242px", width: "300px" }}>
+                {!colorMenu ?
+                <Grid container sx={{ alignItems: "center", justifyContent: "space-evenly", mt: 2 }}>
+                    {Object.keys(iconMap).map((key) => (
+                        <Grid item key={key} xs={4} sx={{ display: "flex", alignItems: "center", justifyContent: "center", mb: 1 }} 
+                            onClick={() => { partition.setPartitionIcon(key); update(); setIconAnchor(null); }}>
+                            <Box className="PartitionIcon">
+                                {iconMap[key]}
+                            </Box>
+                        </Grid>
+                    ))}
+                </Grid> :
+                <Box className="ColorPicker"> 
+                    <HexColorPicker color={partition.getPartitionColor()} onChange={(color) => { 
+                        partition.setPartitionColor(color); 
+                        counter.current++;
+                        if (counter.current > 3) {
+                            update();
+                            counter.current = 0;
+                        } 
+                    }} />
+                    <HexColorInput color={partition.getPartitionColor()} onChange={(color) => { partition.setPartitionColor(color); update(); }} />
+                </Box>
+                }
+            </Box>
         </Stack>
     </Popover>
   );
