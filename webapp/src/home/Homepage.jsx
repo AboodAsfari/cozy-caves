55731a30c61a136a118d6eb66828604738135edd
Gideon Wilkins <wilkingide@ecs.vuw.ac.nz>
2023-09-01 18:08:31 +1200

    Removed test code

--- webapp/src/home/Homepage.jsx
+++ webapp/src/home/Homepage.jsx
@@ -62,19 +62,4 @@ const Homepage = (props) => {
     );
 };
 
-const HomepageGrid = () => {
-    return (
-        <Box display="flex" sx={{ flexGrow: 0}}>
-            <Grid container spacing={2} sx={{marginTop: 10}}>
-                <Grid item xs={6}>
-                    <Typography variant="h5" sx={{ margin: 5}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Typography>
-                </Grid>
-                <Grid item xs={6}>
-                    <Typography variant="h5" sx={{ margin: 5}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Typography>
-                </Grid>
-            </Grid>
-        </Box>
-    );
-};
-
 export default Homepage;
