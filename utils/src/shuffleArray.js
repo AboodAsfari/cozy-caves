dd33ed1ce2cd7f28fc95f4302253948067939b1a
AboodAsfari <abdz2004@gmail.com>
2023-09-03 16:30:43 +1200

    Added an array shuffler to utils

---
+++ utils/src/shuffleArray.js
@@ -0,0 +1,14 @@
+const shuffleArray = (array) => {
+    let currentIndex = array.length,  randomIndex;
+
+    while (currentIndex > 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+    }
+
+    return array;
+}
+
+module.exports = shuffleArray;
