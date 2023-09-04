dde2f5804c195095c57012af7fc061b44bb9e679
AboodAsfari <abdz2004@gmail.com>
2023-09-05 00:47:47 +1200

    Converts HSL to hex so color picker can read it

--- room-generation/src/layout/partition.js
+++ room-generation/src/layout/partition.js
@@ -39,10 +39,21 @@ class Partition {
     constructor() {
         this.resetScaling();
 
-        let hue = 360 * Math.random();
-        let saturation = 100 + 70 * Math.random();
-        let lightness = 70 + 10 * Math.random();
-        this.#partitionColor =  "hsl(" + hue + ',' + saturation + '%,' + lightness + '%)';
+        const hslToHex = (h, s, l) => {
+            l /= 100;
+            const a = s * Math.min(l, 1 - l) / 100;
+            const f = n => {
+                const k = (n + h / 30) % 12;
+                const color = l - a * Math.max(Math.min(k - 3, 9 - k, 1), -1);
+                return Math.round(255 * color).toString(16).padStart(2, '0');   // convert to Hex and prefix "0" if needed
+            };
+            return `#${f(0)}${f(8)}${f(4)}`;
+        }
+
+        let h = 360 * Math.random();
+        let s = 100;
+        let l = 50 + 10 * Math.random();
+        this.#partitionColor = hslToHex(h, s, l);
     }
 
     /**
