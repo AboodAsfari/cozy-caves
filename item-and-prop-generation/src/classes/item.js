c10311f092beb2b33b27728d3e3c5405cd4b1808
“Firstname <batoondian@ecs.vuw.ac.nz>
2023-09-09 23:38:33 +1200

    Make getters for an item's attributes

--- item-and-prop-generation/src/classes/item.js
+++ item-and-prop-generation/src/classes/item.js
@@ -39,6 +39,11 @@ class Item {
     getPosition() { return this.#position; }
     getOffset() { return this.#offset; }
     getRotation() { return this.#rotation; }
+    
+    getName() { return this.name; }
+    getDesc() { return this.desc; }
+    getRarity() { return this.rarity; }
+    getProperties() { return this.properties; }
 
     getName() { return this.name; }
     getDesc() { return this.desc; }
