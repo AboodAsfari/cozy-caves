1654920a4e5d74f86af47dc835001e6a9e26a2b2
AboodAsfari <abdz2004@gmail.com>
2023-09-10 20:36:42 +1200

    Added alt wall tile IDs

--- utils/src/tileID.js
+++ utils/src/tileID.js
@@ -9,6 +9,15 @@ const TileID = {
     FLOOR_2: 7,
     FLOOR_3: 8,
     FLOOR_4: 9,
+    EDGE_WALL_2: 10,
+    EDGE_WALL_3: 11,
+    EDGE_WALL_4: 12,
+    CORNER_WALL_2: 13,
+    CORNER_WALL_3: 14,
+    CORNER_WALL_4: 15,
+    INNER_WALL_2: 16,
+    INNER_WALL_3: 17,
+    INNER_WALL_4: 18
 }
 
 module.exports = TileID;
