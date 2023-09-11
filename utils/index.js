28d9329ffff746e7a052af1e2ea5256aa2fcba3d
Naomi Parte <partenaom@ecs.vuw.ac.nz>
2023-09-11 18:37:05 +1200

    exporting the right util files for rarity

--- utils/index.js
+++ utils/index.js
@@ -1,5 +1,7 @@
 const Point = require("./src/point");
 const TileID = require("./src/tileID");
+const ItemRarity = require("./src/itemRarity");
+const PropRarity = require("./src/propRarity");
 const shuffleArray = require("./src/shuffleArray");
 
-module.exports = { Point, TileID, shuffleArray };
+module.exports = { Point, TileID, ItemRarity, PropRarity, shuffleArray };
