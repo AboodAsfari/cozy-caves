6b0720544e4e1a92ae48a2b3fc306aa5f93fa343
AboodAsfari <abdz2004@gmail.com>
2023-09-22 16:20:28 +1200

    Added TileSpacialType to util exports

--- utils/index.js
+++ utils/index.js
@@ -1,7 +1,8 @@
 const Point = require("./src/point");
 const TileID = require("./src/tileID");
+const TileSpacialType = require("./src/tileSpacialType");
 const ItemRarity = require("./src/itemRarity");
 const PropRarity = require("./src/propRarity");
 const shuffleArray = require("./src/shuffleArray");
 
-module.exports = { Point, TileID, ItemRarity, PropRarity, shuffleArray };
+module.exports = { Point, TileID, TileSpacialType, ItemRarity, PropRarity, shuffleArray };
