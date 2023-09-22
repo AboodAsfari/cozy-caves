43535a571712cdd2dac8f1dcf3e62869735544bc
AboodAsfari <abdz2004@gmail.com>
2023-09-22 15:48:08 +1200

    Added a tile spacial type enum

---
+++ utils/src/tileSpacialType.js
@@ -0,0 +1,13 @@
+const TileSpacialType = {
+    FLOOR: 0,
+    LEFT_EDGE_WALL: 1,
+    RIGHT_EDGE_WALL: 2,
+    TOP_EDGE_WALL: 3,
+    BOTTOM_EDGE_WALL: 4,
+    TOP_LEFT_CORNER_WALL: 5,
+    TOP_RIGHT_CORNER_WALL: 6,
+    BOTTOM_LEFT_CORNER_WALL: 7,
+    BOTTOM_RIGHT_CORNER_WALL: 8
+}
+
+module.exports = TileSpacialType;
