367d0c869c02ab25a98c6a712f851819095ceb89
Gavin Lim <gavinlim04@gmail.com>
2023-09-29 21:15:50 +1300

    Fixed undefined tiles error and started intermediary hallway openings

--- room-generation/src/hallway/hallway.js
+++ room-generation/src/hallway/hallway.js
@@ -7,25 +7,34 @@ class Hallway
     #tilesToOpen = new Map();
     #room = new Room();
     #shape;
+    #previousPosition;
+    #enteredRooms = new Set();
+    #roomEntryPositions = [];
 
     setOverlappingRoom(room) { this.#overlappingRoom = room; }
     setRoom(room) { this.#room = room; }
     setShape(shape) { this.#shape = shape; }
+    setPreviousPosition(position) { this.#previousPosition = position; }
 
     getOverlappingRoom() { return this.#overlappingRoom; }
-    getPossibleTiles() { return this.#possibleTiles; }
-    getTilesToOpen() { return this.#tilesToOpen; }
     getRoom() { return this.#room; }
     getShape() { return this.#shape; }
+    getPossibleTiles() { return this.#possibleTiles; }
+    getTilesToOpen() { return this.#tilesToOpen; }
+    getPreviousPosition() { return this.#previousPosition; }
+    getEnteredRooms() { return this.#enteredRooms; }
+    getRoomEntryPositions() { return this.#roomEntryPositions; }
     
     addPossibleTile(tile) { this.#possibleTiles.push(tile); }
+    addEnteredRoomIndex(roomIndex) { this.#enteredRooms.add(roomIndex); }
+    addRoomEntryPosition(position) { this.#roomEntryPositions.push(position); }
 
     clearPossibleTiles() { this.#possibleTiles = []; }
 
     addTilesToOpen(roomIndex, worldPositions) {
         if (this.#tilesToOpen.has(roomIndex)) {
-            let current = this.#tilesToOpen.get(roomIndex);
-            this.#tilesToOpen.set(roomIndex, [current, worldPositions]);
+            let updatedArray = this.#tilesToOpen.get(roomIndex).push(worldPositions);
+            this.#tilesToOpen.set(roomIndex, updatedArray);
         } else {
             this.#tilesToOpen.set(roomIndex, [worldPositions]);
         }
@@ -37,13 +46,14 @@ class Hallway
         }));
         hallways.forEach(hallway => {
             let otherTilesToOpen = hallway.getTilesToOpen();
-             for (const [key, value] of otherTilesToOpen.entries()) {
-                 if (this.#tilesToOpen.has(key)) {
-                   this.#tilesToOpen.get(key).push(...value);
-                 } else {
-                   this.#tilesToOpen.set(key, [...value]);
-                 }
-               }
+            for (const [key, value] of otherTilesToOpen.entries()) {
+                if (this.#tilesToOpen.has(key)) {
+                    let updated = this.#tilesToOpen.get(key).concat(value);
+                    this.#tilesToOpen.set(key, updated);
+                } else {
+                    this.#tilesToOpen.set(key, value);
+                }
+            }
         });
         return this;
     }
